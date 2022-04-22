3e6bf290196397e743d3e707d9a1550f6054d54f
Arpit Mishra <60821948+arpitm1@users.noreply.github.com>
2022-04-22 16:30:34 +0530

    Command Line Arrguments
    Java program to find out the sum of command line arguments.

---
+++ CLA.java
@@ -0,0 +1,13 @@
+package Unit1;
+
+public class CLA {
+
+	public static void main(String[] args) {
+		   int a = Integer.parseInt(args[0]);
+	        int b = Integer.parseInt(args[1]);
+	        int sum = a + b;
+	        System.out.println("Sum is " + sum);
+	    }
+
+	}
+
