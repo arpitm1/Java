246b128d561bf72fe61703b6a3b2af9f6ef3d040
Arpit Mishra <60821948+arpitm1@users.noreply.github.com>
2022-04-23 23:51:04 +0530

    Set Data
    Java program to create a Room class, the attributes of this class is roomno, roomtype, roomarea and ACmachine. In this class the member functions are setData and displayData. Use member function to set data and display that data using displayData() method.

---
+++ SetData.java
@@ -0,0 +1,28 @@
+package Unit1;
+
+public class program3 {
+
+			int roomNo;
+			String roomType;
+			float roomArea;
+			boolean acMachine;
+			void setData(int rno, String rt, float area, boolean ac) {
+			roomNo = rno;
+			roomType = rt;
+			roomArea = area;
+			acMachine = ac;
+			}
+			void displayData() {
+			System.out.println("The room is " + roomNo);
+			System.out.println("The room Type is " + roomType);
+			System.out.println("The room area is " + roomArea);
+			String s = (acMachine) ? "yes" : "no";
+			System.out.println("The A / c Machine needed " + s);
+			}
+			public static void main(String arg[]) {
+			program3 room1 = new program3();
+			room1.setData(101, "Deluxe", 240.0f, true);
+			room1.displayData();
+			}
+			
+	}
